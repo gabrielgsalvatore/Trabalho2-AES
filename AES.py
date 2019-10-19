3536a40474183e2daeb8d451d3a97ca714683cbe
Gabriel Salvador <gabrielgarciasalvador@outlook.com>
2019-10-18 21:50:25 -0300

    implementado primeiros métodos popular a matriz

--- AES.py
+++ AES.py
@@ -1,9 +1,35 @@
+import binascii
+
+matriz_estado = [None] * 4
 
 
+# def gerar_matriz():
+#     matriz_estado = [None] * 4
+#     for i in range(4):
+#         x[i] = [None] * 4
+#     return x
+
+def gerar_chave(chave):
+    index = 0
+    index_aux = 0
+    matriz_aux = [None] * 4
+    for x in chave:
+        print(index)
+        
+        matriz_aux[index] = binascii.hexlify(x.encode())
+        index = index+1
+        if index == 4:
+            matriz_estado[index_aux] = matriz_aux
+            matriz_aux = [None] * 4
+            index_aux = index_aux + 1
+            index = 0
 
 
 def main():
-    print("Algoritmo AES!")
+    gerar_chave("ABCDEFGHIJKLMNOP")
+
+    for x in matriz_estado:
+        print(x)
 
 if __name__ == "__main__":
     main()
