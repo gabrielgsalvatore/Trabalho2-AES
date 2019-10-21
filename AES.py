9fe6bfb90b2e942b7b7e46a45d2cda136b8218a0
gabrielgsalvatore <gabrielgarciasalvador@outlook.com>
2019-10-21 20:11:06 -0300

    Chave completa

--- AES.py
+++ AES.py
@@ -3,10 +3,10 @@ import numpy as np
 from collections import deque
 
 matriz_estado = [None] * 4
-key_schedule = [None] * 10
+key_schedule = [None] * 11
 s_box = [None] * 16
 round_constant = [None] * 10
-hex2bin_map = {
+hex2bin_map = { #Conversor hex pra bin
    "0":"0000",
    "1":"0001",
    "2":"0010",
@@ -25,7 +25,7 @@ hex2bin_map = {
    "f":"1111",
 }
 
-def gerar_sbox():
+def gerar_sbox(): #Pouplar a s-box
     linha1=['0x63','0x7c','0x77','0x7b','0xf2','0x6b','0x6f','0xc5','0x30','0x01','0x67','0x2b','0xfe','0xd7','0xab','0x76']
     linha2=['0xca','0x82','0xc9','0x7d','0xfa','0x59','0x47','0xf0','0xad','0xd4','0xa2','0xaf','0x9c','0xa4','0x72','0xc0']
     linha3=['0xb7','0xfd','0x93','0x26','0x36','0x3f','0xf7','0xcc','0x34','0xa5','0xe5','0xf1','0x71','0xd8','0x31','0x15']
@@ -59,7 +59,7 @@ def gerar_sbox():
     s_box[14] = linha15
     s_box[15] = linha16
 
-def gerar_roundConstant():
+def gerar_roundConstant(): #Popular a lista de round_constant
     round_constant[0] = '0x01'
     round_constant[1] = '0x02'
     round_constant[2] = '0x04'
@@ -71,12 +71,11 @@ def gerar_roundConstant():
     round_constant[8] = '0x1b'
     round_constant[9] = '0x36'
 
-def gerar_chave(chave):
+def gerar_chave(chave): #Geramos a matriz estado a partir da chave
     index = 0
     index_aux = 0
     matriz_aux = [None] * 4
     for x in chave:      
-        # matriz_aux[index] = binascii.hexlify(x.encode())
         matriz_aux[index] = hex(ord(x))
         index = index+1
         if index == 4:
@@ -84,81 +83,53 @@ def gerar_chave(chave):
             matriz_aux = [None] * 4
             index_aux = index_aux + 1
             index = 0
-    print(matriz_estado)
     gerar_key_schedule()
     
-def gerar_key_schedule():
-    index = 0
+def gerar_key_schedule(): #Geramos o key schedule
     round_key_0 = [None] * 4
-    word_one = [None] * 4
+    word_one = [None] * 4 
     word_two = [None] * 4
     word_three = [None] * 4
     word_four = [None] * 4
-    complete_key = [None] * 4
     for i in range(4):
         key_aux = matriz_estado[i]
-        word_one[index] = key_aux[0]
-        word_two[index] = key_aux[1]
-        word_three[index] = key_aux[2]
-        word_four[index] = key_aux[3]
-        index = index + 1
-    round_key_0[0] = word_one
+        if i == 0:
+            word_one = key_aux #pegar cada palavra da matriz_estado
+        elif i == 1:    
+            word_two = key_aux
+        elif i == 2:
+            word_three = key_aux
+        elif i == 3:    
+            word_four = key_aux
+    round_key_0[0] = word_one #botar as palavras numa lista que vai ser a nossa round_key raiz
     round_key_0[1] = word_two
     round_key_0[2] = word_three
     round_key_0[3] = word_four
-    key_schedule[0] = round_key_0
-    # key_schedule.insert(0,word_one)
-    # key_schedule.insert(1,word_two)
-    # key_schedule.insert(2,word_three)
-    # key_schedule.insert(3,word_four)
+    key_schedule[0] = round_key_0 #botar essa roundkey raiz na keyschedule
     gerar_primeira_round_key()
 
-def gerar_primeira_round_key(): #TODO
-    for roundkey in range(1,10):
-        roundkey_aux = key_schedule[roundkey - 1]
-        print("ROUNDKEYAUX")
-        print(roundkey_aux)
-        word_aux = roundkey_aux[len(roundkey_aux)-1]
-        print("WORDAUX")
-        print(word_aux)
-        print("KEYSCEHDULE")
-        print(key_schedule)
-        word_sbox = [None] * 4
-        d = deque(word_aux)
-        d.rotate(-1)
-        print(d)
-        for i in range(4):
-            valor_lin = d[i][2:3] 
-            valor_col = d[i][3:4] 
-            word_sbox[i] = getSbox(valor_lin, valor_col)
-        print("Word Sbox")
-        print(word_sbox)
-        word_roundkey = getRoundKey(roundkey-1)
-        print("Word Roundkey")
-        print(word_roundkey)
-        first_rk = xorList(word_sbox, word_roundkey)
-        print("Old KeySchedule:")
-        print(key_schedule)
-        print(first_rk)
-        key_schedule[roundkey] = gerar_restantes_round_key(first_rk, roundkey)
-        print("First rk:")
-        
-        print("New KeySchedule:")
-        print(key_schedule)
+def gerar_primeira_round_key(): 
+    for roundkey in range(1,11): #iterar por todas as roundkeys 
+        roundkey_aux = key_schedule[roundkey - 1] #Pega a roundkey anterior para os cálculos
+        word_aux = roundkey_aux[len(roundkey_aux)-1] #Pega a última palavra da roundkey anterior para gerar as primeiras roundkeys
+        word_sbox = [None] * 4 #Inicializando a nossa palavra da s-box
+        d = deque(word_aux) #usando deque para rotacionar a palavra
+        d.rotate(-1) #rotacionando a palavra para a esquerda
+        for i in range(4): #para cada valor hexa da palavra achar a linha e coluna
+            valor_lin = d[i][2:3] #retirando o 0x do hexa e pegando o primeiro valor
+            valor_col = d[i][3:4]  #retirando 0x do hexa e pegando o segundo valor
+            word_sbox[i] = getSbox(valor_lin, valor_col) #populando a word_sbox a partir dos valores hexa 
+        word_roundkey = getRoundKey(roundkey-1) #busca a roundkey constant a partir da rodada
+        first_rk = xorList(word_sbox, word_roundkey) #xor da round constant com a word da sbox
+        final_first_rk = xorList(first_rk, roundkey_aux[0]) #xor do xor anterior com a primeira palavra da roundkey anterior
+        key_schedule[roundkey] = gerar_restantes_round_key(final_first_rk, roundkey) #Gera o restante das palavras desta roundkey passando a rodada e a primeira palavra gerada
 
 def gerar_restantes_round_key(first_rk, roundkey):
-    new_word = [None] * 4
-    new_word[0] = first_rk
-    key_schedule_aux = key_schedule[roundkey-1]
-    for i in range(1,4):
-        print(i)
-        print("KEYSCHEDULEAUX:")
-        print(key_schedule_aux[i])
-        print("NEWWORD")
-        print(new_word[i-1])
-        new_word[i] = xorList(key_schedule_aux[i], new_word[i-1])
-    print("newwordcompleta")
-    print(new_word)
+    new_word = [None] * 4 #inicializa uma nova word
+    new_word[0] = first_rk #define a primeira word como a gerada anteriormente
+    key_schedule_aux = key_schedule[roundkey-1] #para referenciar os valores da roundkey anterior
+    for i in range(1,4): #para cara word na roundkey
+        new_word[i] = xorList(key_schedule_aux[i], new_word[i-1]) #faça um xor entre a palavra anterior dessa roundkey, com a palavra na mesma posição na roundkey anterior
     return new_word
 
 def xorList(fl,sl): #Retorna uma Lista de Word derivada de duas outras Listas de Word
@@ -170,31 +141,31 @@ def xorList(fl,sl): #Retorna uma Lista de Word derivada de duas outras Listas de
         first_hex_sl = sl[i][2:3] #Pega somente o primeiro valor hex (bits esquerda)
         second_hex = fl[i][3:4] #Pega somente o segundo valor hex (bits direita)
         second_hex_sl = sl[i][3:4] #Pega somente o segundo valor hex (bits direita)
-        first_binary = "".join([hex2bin_map[first_hex],hex2bin_map[second_hex]])
+        first_binary = "".join([hex2bin_map[first_hex],hex2bin_map[second_hex]]) #pega os valores binários dos valores hexa
         second_binary = "".join([hex2bin_map[first_hex_sl],hex2bin_map[second_hex_sl]])
-        padding = hex(int(xor(first_binary,second_binary),2))[2:].zfill(2)
-        padding = "0x" + padding
-        xor_list[i] = padding
+        padding = hex(int(xor(first_binary,second_binary),2))[2:].zfill(2) #faz um xor entre os valores binários
+        padding = "0x" + padding #adiciona 0x novamente
+        xor_list[i] = padding #adiciona a nova word na lista
     return xor_list
 
 def xor(bit1,bit2): #Retorna string de bits resultante do xor entre 2 string de bits
     xor = ""
     for x in range(8):
             if(bit1[x] != bit2[x]):
-                xor = xor + "1"
+                xor = xor + "1" #caso diferente retorna 1
             else:
-                xor = xor + "0"
+                xor = xor + "0" #caso igual retorna 0
     return xor
 
 def getRoundKey(rk):
-    word_rk = ['0x00'] * 4
-    word_rk[0] = round_constant[rk]
+    word_rk = ['0x00'] * 4 #inicializa uma lista com 0x00 em todas posiçoes
+    word_rk[0] = round_constant[rk] #popula o primeiro valor com o valor da round_constant respectivo
     return word_rk
 
 def getSbox(valor_lin, valor_col):
     index_lin = 0
     index_col = 0
-    if(valor_lin == 'a'):
+    if(valor_lin == 'a'): #retorna o valor int dos valores hexa
         index_lin = 10
     elif(valor_lin == 'b'):
         index_lin = 11
@@ -222,18 +193,16 @@ def getSbox(valor_lin, valor_col):
         index_col = 15
     else:
         index_col = valor_col
-    index_lin_asint = int(index_lin)   
+    index_lin_asint = int(index_lin) #cast para considerar o valor sempre como uma integer
     index_col_asint = int(index_col) 
 
-    return(s_box[index_lin_asint][index_col_asint])
+    return(s_box[index_lin_asint][index_col_asint]) #retorna o respectivo valor na s_box
 
 
 def main():
     gerar_sbox()
     gerar_roundConstant()
-    print(round_constant)
     gerar_chave("ABCDEFGHIJKLMNOP")
-    print("ASDFKFO")
     print(key_schedule[0])
     print(key_schedule[1])
     print(key_schedule[2])
@@ -244,6 +213,7 @@ def main():
     print(key_schedule[7])
     print(key_schedule[8])
     print(key_schedule[9])
+    print(key_schedule[10])
 
 
 if __name__ == "__main__":
